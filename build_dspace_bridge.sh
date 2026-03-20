2b8be8fedca1b135785729c65cc023c8601cc169
dSPACE-IAC <rheveling@dspace.de>
2026-03-20 14:55:10 +0100

    Add platform specification for Docker builds

--- build_dspace_bridge.sh
+++ build_dspace_bridge.sh
@@ -14,12 +14,12 @@ NAME_FOXGLOVE=$TEAM/iac_ros2_foxglove_bridge:$DATE
 
 # build image
 echo "---------- build general dspace_bridge_dev ----------"
-docker build -t $NAME_DEV --target=dspace_bridge_dev -f Dockerfile ..
+docker build -t $NAME_DEV --target=dspace_bridge_dev --platform=linux/amd64 -f Dockerfile ..
 echo "---------- build asm_socketcan_bridge ----------"
-docker build -t $NAME_ASM_SOCKETCAN --target=asm_socketcan_bridge -f Dockerfile ..
+docker build -t $NAME_ASM_SOCKETCAN --target=asm_socketcan_bridge --platform=linux/amd64 -f Dockerfile ..
 echo "---------- build iac_aurelion_ros2_bridge ----------"
-docker build -t $NAME_IAC_AURELION_ROS2 --target=aurelion_ros2_bridge -f Dockerfile ..
+docker build -t $NAME_IAC_AURELION_ROS2 --target=aurelion_ros2_bridge --platform=linux/amd64 -f Dockerfile ..
 echo "---------- build iac_socketcan_dbw_bridge ----------"
-docker build -t $NAME_IAC_SOCKETCAN_DBW --target=socketcan_dbw_bridge -f Dockerfile ..
+docker build -t $NAME_IAC_SOCKETCAN_DBW --target=socketcan_dbw_bridge --platform=linux/amd64 -f Dockerfile ..
 echo "---------- build ros2_foxglove_bridge ----------"
-docker build -t $NAME_FOXGLOVE --target=ros2_foxglove_bridge -f Dockerfile ..
+docker build -t $NAME_FOXGLOVE --target=ros2_foxglove_bridge --platform=linux/amd64 -f Dockerfile ..
