720c0f855811617ca2acdbc02e78ca779785cc7e
Robin Heveling <rheveling@dspace.de>
2026-04-29 17:18:19 +0200

    add login scripts

---
+++ aws-login.sh
@@ -0,0 +1,4 @@
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin https://722180079256.dkr.ecr.eu-central-1.amazonaws.com
